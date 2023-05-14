43da4ec92fef9609eaebba838ce5d23d70b73a6a
Filip Vukić <filipvukic90@gmail.com>
2023-05-14 23:23:12 +0200

    added profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <>
+      <div className={css.profile}>
+        <div className={css.description}>
+          <img src={avatar} alt="User avatar" className={css.avatar} />
+          <p className={css.name}>{username}</p>
+          <p className={css.tag}>{tag}</p>
+          <p className={css.location}>{location}</p>
+        </div>
+
+        <ul className={css.media}>
+          <li className={css.item}>
+            <span className={css.label}>Followers</span>
+            <span className={css.stats}>{stats.followers}</span>
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Views</span>
+            <span className={css.stats}>{stats.views}</span>
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Likes</span>
+            <span className={css.stats}>{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number.isRequired).isRequired,
+};
+
+export default Profile;
