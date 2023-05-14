dfaa45f68be9b6b2a5ad9075dbe61a1b56d12528
Filip Vukić <filipvukic90@gmail.com>
2023-05-15 01:32:04 +0200

    final ver for review
    - added Profile files
    - added Statistics files
    - added FriendList files
    - added TransactionHistory files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendList.Item';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          isOnline={friend.isOnline}
+          name={friend.name}
+        ></FriendListItem>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.TransactionHistory}>
+      <thead className={css.tableHead}>
+        <tr>
+          <th className={css.tType}>Type</th>
+          <th className={css.tAmount}>Amount</th>
+          <th className={css.tCurrency}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr className={css.tableRow} key={item.id}>
+            <td className={css.tableColumn}>{item.type}</td>
+            <td className={css.tableColumn}>{item.amount}</td>
+            <td className={css.tableColumn}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
